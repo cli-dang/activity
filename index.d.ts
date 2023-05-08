72ffaad9e4f5bda3c1e4704a1dc527f36ebb9087
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-05-08 19:34:41 +0000

    @cli-dang/activity v2.0.8
    - refactoring
    - fixed Types by introducing a namespace Activity

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,20 @@
 import { OftypesError } from 'oftypes'
-export declare function exit( message: MessageArgument, error_type?: Error, exit_code?: number, process_exit?: boolean, mute?: boolean ): Promise<OftypesError | string>
-export declare function stderr( message: MessageArgument, mute?: boolean ): Promise<Buffer | string | OftypesError>
-export declare function trace( ...data: [unknown] ): Promise< null |string | OftypesError>
 
-export declare const trace_options: TraceOptions
+export declare function exit( message: Activity.MessageArgument, error_type?: Error, exit_code?: number, process_exit?: boolean, mute?: boolean ): Promise<OftypesError | string>;
+export declare function stderr( message: Activity.MessageArgument, mute?: boolean ): Promise<Buffer | string | OftypesError>;
+export declare function trace( ...data: [ unknown ] ): Promise<string | OftypesError>;
 
-declare global{
-  type TraceOptions = {
-    mute: boolean,
-    colors: boolean,
-    depth: number,
-    showHidden: boolean
+export declare const trace_options: Activity.TraceOptions
+
+declare global {
+  namespace Activity {
+    type TraceOptions = {
+      mute: boolean,
+      colors: boolean,
+      depth: number,
+      showHidden: boolean
+    }
+    type MessageArgument = string | Buffer;
   }
-  type MessageArgument = string | Buffer;
+
 }
