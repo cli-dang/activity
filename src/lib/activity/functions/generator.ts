481dc2f318a42c778b35cfc779af8721f8712692
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-05-08 19:33:17 +0000

    @cli-dang/activity v2.0.8
    - refactoring
    - fixed Types
    - reduced arguments for AsyncGenerator functions using Class field.

--- src/lib/activity/functions/generator.ts
+++ src/lib/activity/functions/generator.ts
@@ -2,48 +2,60 @@ import { Dang } from '@cli-dang/decors'
 import { error_, number_, oftype_, OftypesError } from 'oftypes'
 import { stderr_exit_text, stderr_trace_text } from './text/code.js'
 
-export default class activity_generator{
+export default class Generator {
 
-  private async * string_buffer( message, module:string, argument:string ):AsyncGenerator{
-    yield await oftype_( message ) === 'Buffer' ||
-    await oftype_( message ) === 'String'
-      ? null
-      : new OftypesError( Dang.b_red( await stderr_exit_text( message, module, argument ) ) )
+  private readonly module: string
+  private argument: string
+  private readonly data: string|Buffer|unknown
+
+  constructor( data: string|Buffer|unknown, module: string, argument: string ) {
+    this.data = data
+    this.module = module
+    this.argument = argument
   }
 
-  public async * type_check( message:Buffer|string, error_type?:undefined|Error, exit_code?:undefined|number ):AsyncGenerator{
+  private async* string_OR_buffer(): AsyncGenerator<null | OftypesError, void, void> {
+    this.argument = 'message'
+    yield await oftype_( this.data, { Buffer: true } )
+      ? null
+      : await oftype_( this.data, { String: true } ) === true
+        ? null
+        : new OftypesError( Dang.b_red( await stderr_exit_text( this.data, this.module, this.argument ) ) )
+  }
 
-    const module:string = error_type ? 'exit' : 'stderr'
+  public async* type_check( error_type?: undefined | Error, exit_code?: undefined | number ): AsyncGenerator<null | Error, void, void> {
 
-    let type:null|Error
-    for await ( const check of this.string_buffer( message, module, 'message' ) ) {
+    let type: null | Error
+    for await ( const check of this.string_OR_buffer() ) {
       if ( check instanceof Error ) {
         type = check
         break
       }
-      type = <null> check
+      type = check
     }
 
     yield type instanceof Error
       ? type
       : null
 
-    if( error_type && exit_code ) {
+    if ( error_type && exit_code ) {
 
+      this.argument = 'error_type'
       yield await error_( error_type )
         ? null
-        : new OftypesError( Dang.b_red( await stderr_exit_text( error_type, module, 'error_type' ) ) )
+        : new OftypesError( Dang.b_red( await stderr_exit_text( error_type, this.module, this.argument ) ) )
 
+      this.argument = 'exit_code'
       yield await number_( exit_code, undefined, undefined, false )
         ? null
-        : new OftypesError( Dang.b_red( await stderr_exit_text( exit_code, module, 'exit_code' ) ) )
+        : new OftypesError( Dang.b_red( await stderr_exit_text( exit_code, this.module, this.argument ) ) )
     }
   }
 
-  public async * boolean( variable:unknown, module ):AsyncGenerator{
+  public async* boolean( variable: unknown ): AsyncGenerator<null | OftypesError, void, void> {
 
-    yield variable.constructor.name === 'Boolean'
+    yield await oftype_( variable, { Boolean: true } ) === true
       ? null
-      : new OftypesError( Dang.b_red( await stderr_trace_text( variable, module ) ) )
+      : new OftypesError( Dang.b_red( await stderr_trace_text( variable, this.module ) ) )
   }
 }
