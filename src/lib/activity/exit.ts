d87b066f2c6b7afe034b7713b06ab542038667ea
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-05-08 19:34:04 +0000

    @cli-dang/activity v2.0.8
    - refactoring
    - fixed Types
    - reduced arguments reflecting the new Generator Class.

--- src/lib/activity/exit.ts
+++ src/lib/activity/exit.ts
@@ -1,29 +1,28 @@
-import { default as Generator } from './functions/generator.js'
 import { OftypesError } from 'oftypes'
+import { default as Generator } from './functions/generator.js'
 import { stderr } from './stderr.js'
 
-export async function exit( message: MessageArgument, error_type = new Error( `${process.title} ♠ error - internal` ), exit_code = 1, process_exit = true, mute = false )
-  :Promise<OftypesError|string> {
+export async function exit( message: Activity.MessageArgument, error_type: Error = new Error( `${ process.title } ♠ error - internal` ), exit_code: number = 1, process_exit: boolean = true, mute: boolean = false ): Promise<OftypesError | string> {
 
-  const generator = new Generator()
-  let type:null|Error
-  for await ( const check of generator.type_check( message, error_type, exit_code ) ) {
+  const generator: Generator = new Generator( message, 'exit', 'exit_code' )
+  let type: null | Error
+  for await ( const check of generator.type_check( error_type, exit_code ) ) {
 
-    if( check instanceof Error ) {
+    if ( check instanceof Error ) {
       type = check
       break
     }
-    type = <null> check
+    type = check
   }
 
-  if( !( type instanceof Error ) && process_exit && ! mute )
+  if ( !( type instanceof Error ) && process_exit && !mute )
     await stderr( `\n${ message }\n\n          [stacktrace]\n          ${ error_type.stack }\n\n` )
 
-  return new Promise( ( resolve, reject ) => {
+  return new Promise( ( resolve, reject ): void => {
 
-    if( type instanceof Error ) reject( type )
+    if ( type instanceof Error ) reject( type )
 
-    else if( ! process_exit  ) resolve( `\n${message}\n\n          [stacktrace]\n          ${error_type.stack}\n exit code -> ${exit_code}\n\n` )
+    else if ( !process_exit ) resolve( `\n${ message }\n\n          [stacktrace]\n          ${ error_type.stack }\n exit code -> ${ exit_code }\n\n` )
 
     else process.exit( exit_code )
   } )
